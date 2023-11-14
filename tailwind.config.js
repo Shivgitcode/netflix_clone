f69a99db170c93172175c1aedbe266e6b692a8f1
shivansh <shivneeraj2004@gmail.com>
2023-11-14 22:28:20 +0530

    this is netflix clone

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        netflix: [
+          "Netflix Sans",
+          "Helvetica Neue",
+          "Segoe UI",
+          "Roboto",
+          "Ubuntu",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
