d5ea1e6acb6ee693cd43e92f582440f694924ff8
shivansh <shivneeraj2004@gmail.com>
2023-11-17 00:12:44 +0530

    added cards

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const button = document.querySelector("#btn");
 
 const div = document.querySelector("#div1");
 
+const span = document.querySelectorAll("span");
+
 button.addEventListener("click", () => {
-  div.classList.toggle("hidden");
-  div.classList.toggle("animate-bounce");
+  div.classList.toggle("scale-y-0");
+  div.classList.toggle("h-[0px]");
 });
