202386646f80018f80fb1b3979517db6b748d45a
shivansh <shivneeraj2004@gmail.com>
2023-11-21 18:34:40 +0530

    netflix-clone done

--- script.js
+++ script.js
@@ -28,11 +28,21 @@ const span = document.querySelector("#span1");
 button.forEach((but, index) => {
   but.addEventListener("click", (e) => {
     const element = but.nextElementSibling;
+    console.log(element);
+    const element2 = element.children;
+    console.log(element2[0]);
     element.classList.toggle("invisible");
-    element.classList.toggle("p-6");
+    // element.classList.toggle("p-6");
     element.classList.toggle("max-h-[75rem]");
     element.classList.toggle("overflow-hidden");
     element.classList.toggle("text-left");
     element.classList.toggle("mb-1");
+
+    element2[0].classList.toggle("invisible");
+    // element2[0].classList.toggle("p-6");
+    element2[0].classList.toggle("max-h-[75rem]");
+    element2[0].classList.toggle("overflow-hidden");
+    element2[0].classList.toggle("text-left");
+    element2[0].classList.toggle("mb-1");
   });
 });
